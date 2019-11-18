3b1e7c1256f49b8b01b5a51e3b543a5c4e68548a
Kieran-C <kieran.caruana@gmail.com>
2019-11-18 15:57:57 +0000

    p5.js added
    Added the p5.js libary files

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 function setup()
 {
-	createCanvas(800, 600);
-  background(255,255,255);
+    createCanvas(displayWidth, displayHeight);
+    background(255,255,255);
 
 }
 
 function draw()
 {
-
+    fill()
+    rect(0,0,dispalyWidth,displayHeight)
 }
